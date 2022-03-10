0bce81f7c45ad384fae207ac1972f80dae8047b3
James <jamesdryden7@gmail.com>
2022-03-10 13:50:20 -0500

    created and finished landing page of website using html, css, js.

---
+++ script.js
@@ -0,0 +1,59 @@
+const body = document.querySelector('body');
+const hamburgerBtn = document.querySelector('#hamburger');
+const overlay = document.querySelector('.overlay');
+const nav_header = document.querySelector('#nav');
+const navList = document.querySelector('#nav-list');
+const navLink = document.querySelectorAll('.list-item');
+
+hamburgerBtn.addEventListener('click', () => {
+	hamburgerBtn.classList.toggle('active');
+
+	if (!overlay.classList.contains('open')) {
+		overlay.classList.add('open');
+	} else {
+		setTimeout(function removeDisplay() {
+			overlay.classList.remove('open');
+		}, 400);
+	}
+
+	if (overlay.classList.contains('fade-in')) {
+		overlay.classList.add('fade-out');
+		overlay.classList.remove('fade-in');
+	} else {
+		overlay.classList.add('fade-in');
+		overlay.classList.remove('fade-out');
+	}
+
+	if (!navList.classList.contains('show')) {
+		navList.classList.add('show');
+		body.classList.add('locked-screen');
+	} else {
+		navList.classList.remove('show');
+		body.classList.remove('locked-screen');
+	}
+});
+
+navLink.forEach((link) => {
+	link.addEventListener('click', () => {
+		navList.classList.remove('show');
+		body.classList.remove('locked-screen');
+		if (!overlay.classList.contains('open')) {
+			overlay.classList.add('open');
+		} else {
+			setTimeout(function removeDisplay() {
+				overlay.classList.remove('open');
+			}, 400);
+		}
+		if (overlay.classList.contains('fade-in')) {
+			overlay.classList.add('fade-out');
+			overlay.classList.remove('fade-in');
+		} else {
+			overlay.classList.add('fade-in');
+			overlay.classList.remove('fade-out');
+		}
+
+		if (hamburgerBtn.classList.contains('active')) {
+			hamburgerBtn.classList.remove('active');
+		}
+	});
+});
